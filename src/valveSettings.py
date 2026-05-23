160c6d5f1a3a0ad566e2a00442ca72490ac647be
schneemaier <akos.schneemaier@gmail.com>
2026-05-22 21:40:10 -0400

    Adding load and save routines to the valveSettings.py

--- src/valveSettings.py
+++ src/valveSettings.py
@@ -3,11 +3,14 @@ import os
 import logging
 
 class valveSettings:
-    def __init__(self, valveSettings_path='valveSettings.json', scheduleSettings_path='scheduleSettings.json'):
+    #def __init__(self, valveSettings_path='valveSettings.json', scheduleSettings_path='scheduleSettings.json'):
+    def __init__(self):
         self.controllerMac = []
         self.valveUnits = {}
         self.schedule = {}
+        self.load()
 
+    def load(self, valveSettings_path='valveSettings.json', scheduleSettings_path='scheduleSettings.json'):
         if os.path.exists(valveSettings_path):
             with (open(valveSettings_path, 'r') as f):
                 vs = json.load(f)
@@ -31,6 +34,38 @@ class valveSettings:
                         #    print(data.get('valveUnit'),day["day"],"valve1",valve1[c]["period"])
                         #    print(data.get('valveUnit'),day["day"],"valve2",valve2[c]["period"])
                     self.schedule[vUnit] = scheduleDay
+
+    def save(self, valveSettings_path='valveSettings.json', scheduleSettings_path='scheduleSettings.json'):
+        if os.path.exists(valveSettings_path):
+            vs = {
+                "data": [
+                    {"controllerMac": mac, "valveUnits": self.valveUnits[mac]}
+                    for mac in self.controllerMac
+                ]
+            }
+            with open(valveSettings_path, 'w') as f:
+                json.dump(vs, f, indent=4)
+        if os.path.exists(scheduleSettings_path):
+            ss = {"data": []}
+            for vUnit, scheduleDay in self.schedule.items():
+                schedule_list = []
+                for d, valves in scheduleDay.items():
+                    schedule_list.append({
+                        "day": d,
+                        "valve1": valves[0],
+                        "valve2": valves[1],
+                        "valve3": valves[2],
+                        "valve4": valves[3]
+                    })
+
+                ss["data"].append({
+                    "valveUnit": vUnit,
+                    "schedule": schedule_list
+                })
+
+            # Save the reconstructed data to the JSON file
+            with open(scheduleSettings_path, 'w') as f:
+                json.dump(ss, f, indent=4)
 
 valveSettings = valveSettings()
 
